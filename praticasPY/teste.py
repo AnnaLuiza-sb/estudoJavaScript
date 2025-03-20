b7a7571b5b41f68af3407498752860d5486174a4
Anna Luiza <luiza.sobral.ls@gmail.com>
2025-03-20 15:50:49 -0300

    envio inicial

---
+++ praticasPY/teste.py
@@ -0,0 +1,69 @@
+# print("hello world")
+
+
+# numero1 = int(input("Digite um número"))
+# print("O número informado foi",  numero1)
+
+
+# num1 = int(input("Digite um número"))
+# num2 = int(input("Digite outro número"))
+
+# soma = num1 + num2
+# print("A soma dos números é igual a", soma)
+
+
+# nota1 = float(input("Digite sua 1º nota"))
+# nota2 = float(input("Digite sua 2º nota"))
+# nota3 = float(input("Digite sua 3º nota"))
+# nota4 = float(input("Digite sua 4º nota"))
+# aluno = "Luiza"
+# media = (nota1 + nota2 + nota3 + nota4)/4
+
+# print("A média da aluna", aluno, "foi", media)
+
+# metros = float(input("Digite um número em metros"))
+
+# centimentros = metros * 100
+# print("O valor em centímetros é", centimentros)
+
+# raio = float(input("informe o valor do raio"))
+# pi = 3.14
+# area_circulo = pi * (raio**2)
+
+# print("A área do círcula é igual a", area_circulo)
+
+# lado1 = int(input("Informe o valor do lado 1"))
+# lado2 = int(input("Informe o valor do lado 2"))
+# area_quadrado = lado1 * lado2
+# quadrado_area = area_quadrado * 2
+
+# print("A área do quadrado é", area_quadrado, "e o quadrado da área é igual a", quadrado_area)
+
+
+# lucro_por_hora = float(input('Quanto você ganha por hora'))
+
+# horas_trabalho = float(input('Qual o seu total de horas trabalhadas no mês'))
+
+# print ('Seu sálario por mês é equivalente a', lucro_por_hora * horas_trabalho)
+
+# celsius = float(input("Informe a temperatura em °C: "))
+# fahrenheit = (celsius * 1.8) + 32
+# print('A temperatura de {} ºC corresponde a {} ºF!'.format(celsius, fahrenheit))
+
+# fahrenheit = float(input("Informe a temperatura em °F: "))
+# celsius = 5/9 * (fahrenheit - 32)
+# print('A temperatura de {} ºF corresponde a {} ºC!'.format(fahrenheit, celsius))
+
+# num_I = int(input('Digite um número inteiro'))
+# num_I2 = int(input('Digite outro número inteiro'))
+# num_R = float(input('Digite um número real'))
+
+# produto_dobro = (num_I * 2) * (num_I2/2)
+# triplo_soma = (num_I * 3) + num_R
+# potencia = (num_R)**3
+
+# print(produto_dobro)
+# print(triplo_soma)
+# print(potencia)
+
+
